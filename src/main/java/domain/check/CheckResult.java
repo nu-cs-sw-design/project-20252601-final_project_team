181227227967f9632ece63441e74ba52a3d08911
谭苏川 <2428935840@qq.com>
2025-11-28 14:38:41 -0600

    Complete Poor Cohesion Chekc.

--- src/main/java/domain/check/CheckResult.java
+++ src/main/java/domain/check/CheckResult.java
@@ -1,15 +1,14 @@
 package domain.check;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class CheckResult {
     public String checkName;
     public String className;
     public boolean result;
     public String message;
 
-    public CheckResult(String checkName, String className, boolean result, String message) {
-        this.checkName = checkName;
-        this.className = className;
-        this.result = result;
-        this.message = message;
-    }
 }
