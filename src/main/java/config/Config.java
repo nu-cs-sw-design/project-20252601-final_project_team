9d8024e14b9484fa730aa9fc5fb70c453c21091f
谭苏川 <2428935840@qq.com>
2025-12-01 14:57:40 -0600

    Edit Main.

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,9 +1,8 @@
 package config;
 
 public class Config {
-    public static final String PROJECT_PATH = "";
+    public static String PROJECT_PATH = "target/classes";
     public static final String FILE_GENERATION_PATH = "";
-    public static final String API_URL = "";
     public static final String API_KEY = "";
 
 }
--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -3,8 +3,12 @@ package presentation;
 import config.Config;
 import domain.ProjectAnalyzer;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Config.PROJECT_PATH = in.nextLine();
         ProjectAnalyzer analyzer = new ProjectAnalyzer(Config.PROJECT_PATH);
         System.out.println(analyzer.analyze());
     }
